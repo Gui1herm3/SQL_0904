a71cb17117a11a652d1af9feecb544a2cf0c2446
Jao <125588400+Gui1herm3@users.noreply.github.com>
2024-04-09 11:09:38 -0300

    Update atvidade.sql

--- atvidade.sql
+++ atvidade.sql
@@ -1,7 +1,32 @@
 SELECT * FROM `SALARIOS` WHERE profissão ='Profissional de T.I.';
+
 SELECT * FROM `SALARIOS` WHERE profissão LIKE 'P%';
-SELECT SUM(salario) AS SOMASAL_PROF 								   FROM SALARIOS WHERE profissão = 'professor'}
-SELECT AVG(salario) AS media_salarios								  FROM SALARIOS 											  WHERE profissão = 'Profissional de T.I.' AND 						       	nome IN ('Lucas Costa', 'Roberto Silva')
-SELECT MAX(salario) AS maior_sal_ti								  FROM SALARIOS										     	   WHERE profissão = 'Profissional de T.I.' AND					 	          nome IN ('Lucas Costa', 'Roberto Silva')
-SELECT MAX(salario) AS maior_sal_ti=								  FROM SALARIOS 											  WHERE profissão IN ('Ator','Professor')
-SELECT MAX(salario) AS maior_sal_ti 								  FROM SALARIOS											  WHERE profissão IN ('Ator','Professor','ATRIZ')
+
+--SOMA SALÁRIOS
+SELECT SUM(salario) AS SOMASAL_PROF 	
+FROM SALARIOS WHERE profissão = 'professor'}
+
+--MÉDIA 
+SELECT AVG(salario) AS media_salarios			
+FROM SALARIOS 											  
+WHERE profissão = 'Profissional de T.I.' AND 						      
+nome IN ('Lucas Costa', 'Roberto Silva')
+
+--MÁXIMO
+SELECT MAX(salario) AS maior_sal_ti							 
+FROM SALARIOS										     	   
+WHERE profissão = 'Profissional de T.I.' AND					 	         
+nome IN ('Lucas Costa', 'Roberto Silva')
+
+--MÍNIMO
+SELECT MIN(salario) AS MENOS_SALARIO FROM `SALARIOS`WHERE profissão = 'Professor'; 
+
+--DESAFIO 1
+SELECT MAX(salario) AS maior_sal_ti=								  
+FROM SALARIOS 											  
+WHERE profissão IN ('Ator','Professor')
+
+--DESAFIO 2
+SELECT MAX(salario) AS maior_sal_ti 								  
+FROM SALARIOS											  
+WHERE profissão IN ('Ator','Professor','ATRIZ')
